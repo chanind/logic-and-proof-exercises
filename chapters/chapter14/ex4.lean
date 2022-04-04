350e0f64e1edfa067f5d8c6f9ae900f23ee48ea8
David Chanin <chanindav@gmail.com>
2022-04-04 14:47:24 +0100

    briefer version of ex4

--- chapters/chapter14/ex4.lean
+++ chapters/chapter14/ex4.lean
@@ -9,6 +9,7 @@ sorry
 
 open nat
 
+-- using the methods from the chapter
 example : 1 ≤ 4 :=
   have 1 < 2, from nat.lt_succ_self 1,
   have 2 < 3, from nat.lt_succ_self 2,
@@ -16,3 +17,6 @@ example : 1 ≤ 4 :=
   have 1 < 3, from nat.lt_trans ‹1 < 2› ‹2 < 3›,
   have 1 < 4, from nat.lt_trans ‹1 < 3› ‹3 < 4›,
   show 1 ≤ 4, from nat.le_of_lt ‹1 < 4›
+
+-- Could also have just done the following, much more brief
+example : 1 ≤ 4 := nat.le_add_right 1 3
